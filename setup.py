c250d745c8b46c853acc5e32476ca06894a20723
Adam Wentz <adam@adamwentz.com>
2016-03-27 15:36:48 -0500

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='keras-vgg-buddy',
-    version='0.0.1',
+    version='0.0.2',
     description='Your pal when you want some VGG16 with your Keras.',
     author='Adam Wentz',
     author_email='adam@adamwentz.com',
